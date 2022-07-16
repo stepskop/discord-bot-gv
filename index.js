2fce175be5f4475b7c175d414de9fa3a838a08c1
StepSkop <74026500+StepSkop@users.noreply.github.com>
2022-07-16 20:04:41 +0200

    GG

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const client = new Client( {intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const mongoose = require('mongoose')
 const config = require('./config.json')
 const dotenv = require('dotenv')
-const { MongoClient } = require('mongodb')
 dotenv.config()
 //Start
 client.on('ready', () => {
     const guild = client.guilds.resolve("712268262347374632")
     client.user.setActivity("/help", { type: 'LISTENING'})
-    console.log("OK, running v1.1")
-    
-    const database = new MongoClient(process.env.MONGO_SRV)
-
+    console.log("OK")
+ 
     const command = require('./src/commandsBuilder')
     command(config, client, 'clearComm', 1, ' ', (message, args) => {
         if (message.author.id === "294676882081972226") {
@@ -24,7 +21,7 @@ client.on('ready', () => {
     slashCommands(config, client)
     //Game Deals
     const gameDeals = require('./src/gameDeals')
-    gameDeals(config, client, database)
+    gameDeals(config, client)
     //Role select
     const roleSelect = require('./src/roleSelect');
     roleSelect(config, client)
@@ -39,10 +36,10 @@ const { YtDlpPlugin } = require('@distube/yt-dlp')
 const { VoiceConnection } = require('@discordjs/voice')
 client.distube = new DisTube(client, {
     youtubeDL: false,
-    // leaveOnEmpty: true,
-    // leaveOnStop: true,
-    // leaveOnFinish: true,
-    plugins: [new YtDlpPlugin({update: true}), new SpotifyPlugin()]
+    leaveOnEmpty: true,
+    leaveOnStop: true,
+    leaveOnFinish: true,
+    plugins: [new YtDlpPlugin(), new SpotifyPlugin()]
 })
 //Distube - music
 const disTubeInfo = require('./src/distube')
@@ -55,5 +52,3 @@ const onJoin = require('./src/onJoin')
 onJoin(config, client)
 //Auth login
 client.login(process.env.TOKEN)
-
-
