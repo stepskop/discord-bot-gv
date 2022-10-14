70455e7821a420325aa0933c5e321edd7be49242
StepSkop <74026500+StepSkop@users.noreply.github.com>
2022-10-14 11:36:37 +0200

    Edited for limited hosting

--- src/onJoin.js
+++ src/onJoin.js
@@ -1,4 +1,4 @@
-const {} = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 module.exports = (config, client) => {
     client.on('guildMemberAdd', guildMember => {
         console.log('User @' + guildMember.user.tag + ' has joined the server!');
@@ -8,5 +8,6 @@ module.exports = (config, client) => {
             const role = guildMember.guild.roles.cache.find((role) => role.id == element)
             guildMember.roles.add(role);
         });
+        guildMember.send({embeds: [new MessageEmbed().setTitle('Vítej na GameVibe').setColor('DARK_PURPLE').setDescription('Doufám že se ti tu bude líbit' + '\n\n' + 'Aby ostaní věděli co hraješ, vyber si role v **#🎮game-select🎮**' + '\n\n' + '*GameVibe*')]})
     });
 }
