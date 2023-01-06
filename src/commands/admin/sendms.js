736fe03a4d959549f2c4f254c4f94566d0fd3585
StepSkop <74026500+StepSkop@users.noreply.github.com>
2023-01-06 20:27:59 +0100

    Fixed 'Send DM's'

--- src/commands/admin/sendms.js
+++ src/commands/admin/sendms.js
@@ -1,35 +1,39 @@
 module.exports = (config, client, interaction, guild, options) => {
-    if (interaction.member.id === config.admin) {
-        switch (options.getSubcommand()) {
-            case 'user':
-                const userArg = options.getUser('user')
-                const messageUserArg = options.get('message')
-                userArg.send(messageUserArg.value)
-
-                return interaction.reply({
-                    content:'Succesfully sent to ' + "<@"+ userArg + ">",
-                    ephemeral:true
-                })
-            case 'role':
-                const roleArg = options.get('role')
-                const messageRoleArg = options.get('message')
-                guild.members.fetch( {force: true} ).then(user => {
-                    user.forEach(user => {
-                        if (user.roles.cache.has(roleArg.value) == true) {
-                            //console.log(user.user.username)
-                            user.send(messageRoleArg.value)
-                        }
+    try {
+        if (interaction.member.id === config.admin) {
+            switch (options.getSubcommand()) {
+                case 'user':
+                    const userArg = options.getUser('user')
+                    const messageUserArg = options.get('message')
+                    userArg.send(messageUserArg.value)
+    
+                    return interaction.reply({
+                        content:'Succesfully sent to ' + "<@"+ userArg + ">",
+                        ephemeral:true
                     })
-                });
-                return interaction.reply({
-                    content:'Succesfully sent to '+ "<@&"+ roleArg.value +">",
-                    ephemeral:true
-                })
+                case 'role':
+                    const roleArg = options.get('role')
+                    const messageRoleArg = options.get('message')
+                    guild.members.fetch( {force: true} ).then(user => {
+                        user.forEach(user => {
+                            if (user.roles.cache.has(roleArg.value) == true) {
+                                //console.log(user.user.username)
+                                user.send(messageRoleArg.value)
+                            }
+                        })
+                    });
+                    return interaction.reply({
+                        content:'Succesfully sent to '+ "<@&"+ roleArg.value +">",
+                        ephemeral:true
+                    })
+            }
+        } else {
+            interaction.reply({
+                content: "You are not able to send DM's now",
+                ephemeral: true
+            })
         }
-    } else {
-        interaction.reply({
-            content: "You are not able to send DM's now",
-            ephemeral: true
-        })
+    } catch (error) {
+        console.log(error);
     }
 }
